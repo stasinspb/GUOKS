d7c1ffd66ce9c02f40f3b3188697e440d3324de9
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-13 21:20:08 +0300

    guoks-to-pdf.py

--- guoks-to-pdf.py
+++ guoks-to-pdf.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 uploaded_zip = st.file_uploader("Загрузите ZIP-файл", type=["zip"])
-    if uploaded_zip is not None:
-        zf = zipfile.ZipFile(uploaded_zip)
-        zf.extractall(".")
+if uploaded_zip is not None:
+    zf = zipfile.ZipFile(uploaded_zip)
+    zf.extractall(".")
