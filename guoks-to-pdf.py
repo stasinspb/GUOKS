8a46c4b593ce0a0ca25aa1ad7c7fed1029bcb2a9
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-13 22:20:30 +0300

    guoks-to-pdf.py

--- guoks-to-pdf.py
+++ guoks-to-pdf.py
@@ -17,19 +17,19 @@ if uploaded_zip is not None:
     zf.extractall(dir_path)
     zf.close()
 
-# for root, dirs, files in os.walk(dir_path):  # бежим по папкам
-#     for a in dirs:
-#         if a[:6] != 'GKUOKS' and a[:7] != 'Applied':  # ищем и удаляем все папки, которые не начинаются
-#             shutil.rmtree(os.path.join(root, a))  # на GKUOKS или Applied, т.е. остаются только папки GKUOKS
-#         else:
-#             continue
+for root, dirs, files in os.walk(dir_path):  # бежим по папкам
+    for a in dirs:
+        if a[:6] != 'GKUOKS' and a[:7] != 'Applied':  # ищем и удаляем все папки, которые не начинаются
+            shutil.rmtree(os.path.join(root, a))  # на GKUOKS или Applied, т.е. остаются только папки GKUOKS
+        else:
+            continue
 
-# for root, dirs, files in os.walk(dir_path):  # снова бежим по папкам, удаляем sig, log, txt
-#     for file in files:
-#         if file.endswith('.sig') or \
-#                 file.endswith('.log') or \
-#                 file.endswith('.txt'):
-#             os.remove(os.path.join(root, file))
+for root, dirs, files in os.walk(dir_path):  # снова бежим по папкам, удаляем sig, log, txt
+    for file in files:
+        if file.endswith('.sig') or \
+                file.endswith('.log') or \
+                file.endswith('.txt'):
+            os.remove(os.path.join(root, file))
                     
 try:
     for root, dirs, files in os.walk(dir_path):                    # снова бежим по папкам, перекодируем имена файлов, если такое нужно
