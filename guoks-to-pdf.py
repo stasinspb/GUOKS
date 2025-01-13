86d5d00f4c8a71a727da780701dc4e7c52d142ca
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-13 23:27:27 +0300

    guoks-to-pdf.py

--- guoks-to-pdf.py
+++ guoks-to-pdf.py
@@ -116,10 +116,11 @@ for roots, dirs, files in os.walk(dir_path):
             writer_output = PdfWriter()
             for current_page in range(len(reader_input.pages)):
                 writer_output.addpage(reader_input.pages[current_page])
-            
+            st.write("Текст добавлен")
             for p in geo:                                           # добавляем файлы с геодезией
                 st.write(os.path.join(roots, p))
                 reader_input = PdfReader(os.path.join(roots, p))
+                st.write("Гео добавлен")
                 for current_page in range(len(reader_input.pages)):
                     writer_output.addpage(reader_input.pages[current_page])
 
