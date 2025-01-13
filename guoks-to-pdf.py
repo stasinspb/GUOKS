f00e5e2869a35d760de63261a7fae3afe38dbf3e
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-13 21:37:17 +0300

    guoks-to-pdf.py

--- guoks-to-pdf.py
+++ guoks-to-pdf.py
@@ -5,7 +5,8 @@ import os
 uploaded_zip = st.file_uploader("Загрузите ZIP-файл", type=["zip"])
 if uploaded_zip is not None:
     zf = zipfile.ZipFile(uploaded_zip)
-    zf.extractall(".")
+    os.makedirs('GUOKS')
+    zf.extractall(os.path.join(os.getcwd(),'GUOKS'))
     zf.close()
 st.success("Распаковано")
 st.success(os.getcwd())
