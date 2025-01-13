929e91a54eaa30788c26c139306b9e88883fc989
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-13 21:29:54 +0300

    guoks-to-pdf.py

--- guoks-to-pdf.py
+++ guoks-to-pdf.py
@@ -5,3 +5,4 @@ uploaded_zip = st.file_uploader("Загрузите ZIP-файл", type=["zip"])
 if uploaded_zip is not None:
     zf = zipfile.ZipFile(uploaded_zip)
     zf.extractall(".")
+st.success("Загружено")
