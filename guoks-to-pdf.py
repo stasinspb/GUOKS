1e9a5b84403877a6305a11145d6fa6af5b2db3a8
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-14 11:28:14 +0300

    guoks-to-pdf.py

--- guoks-to-pdf.py
+++ guoks-to-pdf.py
@@ -18,12 +18,12 @@ if uploaded_zip is not None:
     zf.extractall(dir_path)
     zf.close()
 
-    for root, dirs, files in os.walk(dir_path):  # бежим по папкам
-        for a in dirs:
-            if a[:6] != 'GKUOKS' and a[:7] != 'Applied':  # ищем и удаляем все папки, которые не начинаются
-                shutil.rmtree(os.path.join(root, a))  # на GKUOKS или Applied, т.е. остаются только папки GKUOKS
-            else:
-                continue
+    # for root, dirs, files in os.walk(dir_path):  # бежим по папкам
+    #     for a in dirs:
+    #         if a[:6] != 'GKUOKS' and a[:7] != 'Applied':  # ищем и удаляем все папки, которые не начинаются
+    #             shutil.rmtree(os.path.join(root, a))  # на GKUOKS или Applied, т.е. остаются только папки GKUOKS
+    #         else:
+    #             continue
     
     for root, dirs, files in os.walk(dir_path):  # снова бежим по папкам, удаляем sig, log, txt
         for file in files:
