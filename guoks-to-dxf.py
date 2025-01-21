57341fcb1d466a785b616461c8dcc582e2d27ad6
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-21 16:44:28 +0300

    guoks-to-dxf.py

--- guoks-to-dxf.py
+++ guoks-to-dxf.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import zipfile
 import os
-#import shutil
+import shutil
 #import xml.etree.cElementTree as ET
 
 st.title("Создание файла Autocad (dxf) из zip-архивов технических планов зданий и сооружений")
