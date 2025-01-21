519482c24447763674df862a9a4258f26e90b123
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-21 17:41:33 +0300

    guoks-to-dxf.py

--- guoks-to-dxf.py
+++ guoks-to-dxf.py
@@ -23,6 +23,7 @@ def extract_zip_with_directories(zip_path, extract_to):
 
 st.title("Создание файла Autocad (dxf) из zip-архивов технических планов зданий и сооружений")
 uploaded_files = st.file_uploader("Загрузите ZIP-файлы технических планов", type=["zip"], accept_multiple_files=True)
+os.makedirs('DXF')
 st.write(os.listdir())
 
    
