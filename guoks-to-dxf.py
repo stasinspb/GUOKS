f4e03d97093cc4a8863118b633d302565bbca835
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-21 16:30:38 +0300

    guoks-to-dxf.py

--- guoks-to-dxf.py
+++ guoks-to-dxf.py
@@ -4,23 +4,7 @@ import os
 #import shutil
 #import xml.etree.cElementTree as ET
 
-#--------------------------
-def extract_zip_with_directories(zip_path, extract_to):
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        for member in zip_ref.infolist():
-            fixed_path = member.filename.replace('\\', '/')
-            target_path = os.path.join(extract_to, fixed_path)
-            
-            # Проверяем, является ли это папкой или файлом
-            if member.is_dir():
-                os.makedirs(target_path, exist_ok=True)
-            else:
-                os.makedirs(os.path.dirname(target_path), exist_ok=True)
-                with open(target_path, 'wb') as f:
-                    f.write(zip_ref.read(member.filename))
-
-#--------------------------
-
+st.cache.clear()
 st.title("Создание файла Autocad (dxf) из zip-архивов технических планов зданий и сооружений")
 uploaded_files = st.file_uploader("Загрузите ZIP-файлы технических планов", type=["zip"], accept_multiple_files=True)
 for uploaded_file in uploaded_files:
