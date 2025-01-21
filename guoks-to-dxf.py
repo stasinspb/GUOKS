1286db66c6c9e02c15682237f4c7d13d48deaeae
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-21 16:52:38 +0300

    guoks-to-dxf.py

--- guoks-to-dxf.py
+++ guoks-to-dxf.py
@@ -6,9 +6,9 @@ import shutil
 
 for file in os.listdir():
     if file not in [".git", "guoks-to-dxf.py", "requirements.txt", ".streamlit", "guoks-to-pdf.py"]:
-        st.write(file)
+        shutil.rmtree(os.path.join(os.getcwd(), file))
     
-
+st.write(os.listdir())
 
 st.title("Создание файла Autocad (dxf) из zip-архивов технических планов зданий и сооружений")
 # #uploaded_files = st.file_uploader("Загрузите ZIP-файлы технических планов", type=["zip"], accept_multiple_files=True)
