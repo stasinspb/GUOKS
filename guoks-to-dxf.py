1376ebbbcfe4ec978e5ea376695dd4c4157e628c
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-21 16:35:01 +0300

    guoks-to-dxf.py

--- guoks-to-dxf.py
+++ guoks-to-dxf.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from streamlit import caching
 import zipfile
 import os
 #import shutil
 #import xml.etree.cElementTree as ET
 
-caching.clear_cache()
+st.cache_data.clear()
 st.title("Создание файла Autocad (dxf) из zip-архивов технических планов зданий и сооружений")
 uploaded_files = st.file_uploader("Загрузите ZIP-файлы технических планов", type=["zip"], accept_multiple_files=True)
 for uploaded_file in uploaded_files:
