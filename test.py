d668759e0fc1d9e475a77a2b49e73e82ea1dc6f1
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-21 20:35:58 +0300

    test.py

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 import streamlit as st
 import os
 st.write(os.listdir())
+# Initialize session state
+st.session_state.text = st.session_state.get('text', 'original')
+
+if st.button("show"):
+    # Allow the user to modify the text
+    st.text_input("Edit Text", key='text')        # <--- set using the key kwarg
+
+# Display the modified text
+st.markdown(st.session_state.text)
+
+if st.button("show again"):
+    # Display the modified text
+    st.markdown(st.session_state.text)
