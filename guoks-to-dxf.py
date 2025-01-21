49d86f89fcb21cf7e4427a04b4a2a7dc3f0446d6
stasinspb <145358266+stasinspb@users.noreply.github.com>
2025-01-21 17:34:27 +0300

    guoks-to-dxf.py

--- guoks-to-dxf.py
+++ guoks-to-dxf.py
@@ -2,13 +2,7 @@ import streamlit as st
 import zipfile
 import os
 import shutil
-#import xml.etree.cElementTree as ET
-
-# for file in os.listdir():
-#     if file not in [".git", "guoks-to-dxf.py", "requirements.txt", ".streamlit", "guoks-to-pdf.py"]:
-#         shutil.rmtree(os.path.join(os.getcwd(), file))
-    
-# st.write(os.listdir())
+import xml.etree.cElementTree as ET
 
 #--------------------------
 def extract_zip_with_directories(zip_path, extract_to):
@@ -27,38 +21,21 @@ def extract_zip_with_directories(zip_path, extract_to):
 
 #--------------------------
 
-
 st.title("Создание файла Autocad (dxf) из zip-архивов технических планов зданий и сооружений")
 uploaded_files = st.file_uploader("Загрузите ZIP-файлы технических планов", type=["zip"], accept_multiple_files=True)
-
 if uploaded_files is not None:
     if os.path.exists('DXF'):
         shutil.rmtree(os.path.join(os.getcwd(),'DXF'))
     os.makedirs('DXF')
-    st.write(os.listdir())
+    dir_path = os.path.join(os.getcwd(),'DXF')
+
+    #---------------------------------------
     for uploaded_file in uploaded_files:
-        os.makedirs(os.path.join(os.getcwd(), 'DXF', uploaded_file.name))
-        dir_path = os.path.join(os.getcwd(),'GUOKS', uploaded_file.name)
-        #---------------------------------------
         extract_zip_with_directories(uploaded_file, dir_path)
-        #---------------------------------------
+    st.write(os.listdir())
+    #---------------------------------------
 
-for root, dirs, files in os.walk('GUOKS'): 
-    for file in files:
-        st.write(file)
-try:
-    shutil.rmtree(os.path.join(os.getcwd(),'GUOKS'))
-except:
-    pass
-st.write(os.listdir())
-# os.remove("GKUOKS_0cc7df6c-a687-4556-94d4-0cb4df49abf9.xml.sig")
-# st.write(os.listdir())
+   
 
-# for uploaded_file in uploaded_files:
-#     st.write(uploaded_file.name)
-#     if os.path.exists(uploaded_file.name):
-#         shutil.rmtree(os.path.join(os.getcwd(), uploaded_file.name))
-#     #os.makedirs(uploaded_file.name)
-#     #with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
-#         #zip_ref.extractall(uploaded_file.name)
-# st.write(os.listdir())
+
+
